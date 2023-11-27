2cd7c591c020ae7be0d684b498aae32c58723229
Ryanwang724 <ryanwang724@gmail.com>
2023-11-28 03:47:48 +0800

    w12 first commit, undone

---
+++ week12_1127/queue.c
@@ -0,0 +1,110 @@
+#include "queue.h"
+#include "space.h"
+
+
+tQueue* createQueue(void){    
+    tQueue *queue;
+    queue=(tQueue *) malloc (sizeof(tQueue));
+
+    if (queue)
+    {
+        queue->front=NULL;
+        queue->rear=NULL;  
+        queue->count=0;
+    }
+
+    return queue;
+}
+
+int enqueue_node(tQueue *queue, int id, int score, int data_type)
+{
+    tQueueNode *newptr = NULL;
+    int mem_location;
+
+    our_malloc (data_type, &newptr, &mem_location);
+    
+    if (newptr == NULL)
+    {
+        printf("    Enqueue False!!! \n");
+        return 0;
+    }
+
+    newptr->next = NULL; //set the value
+    newptr->prev = NULL;
+    newptr->data_type = data_type;
+    newptr->id = id;
+    newptr->location = mem_location;
+
+    if(queue->count == 0)
+    {
+        queue->front = newptr;
+        queue->rear = newptr;
+    }
+    else
+    {
+        tQueueNode *ptr = queue->front;
+        while(ptr->next != NULL)
+        {
+            ptr = ptr->next;  //go to target location
+        }
+        ptr->next = newptr;
+        newptr->prev = ptr;
+        queue->rear = newptr;
+    }
+    queue->count++;
+    return 1;
+}
+
+void dequeue_node(tQueue *queue, tQueueNode *target, int data_type)
+{
+    if(queue->count == 1)
+    {
+        queue->front = NULL;
+        queue->rear = NULL;
+    }
+    else if(target->next == NULL) //last one node
+    {
+        queue->rear = target->prev;
+        target->prev->next = NULL;
+    }
+    else   //middle node
+    {
+        target->next->prev = target->prev;
+        target->prev->next = target->next;
+    }
+    target->next = NULL;
+    target->prev = NULL;
+
+    queue->count--;
+    our_free(data_type, target->location);
+}
+
+tQueueNode *find_target_node(tQueue *queue, int id, int data_type)
+{
+    int i;
+    tQueueNode *target = queue->front;
+    while(target != NULL)
+    {
+        if(target->id == id && target->data_type == data_type)
+        {
+            return target;
+        }
+        target = target->next;
+    }
+    return NULL;
+}
+
+
+void print_queue (tQueue *queue)
+{
+    int i;
+    tQueueNode *target = queue->front;
+
+    printf("      queue content: ");    
+    for (i = 0; i < queue->count; i++)
+    {
+        printf ("%d(%d) ", target->id, target->data_type);
+        target = target->next;
+    }
+    printf("\n");
+}
---
+++ week12_1127/space.c
@@ -0,0 +1,143 @@
+#include "space.h"
+
+unsigned char buffer[ELEMENT_SIZE*NUM_BYTE_BUF];
+unsigned char byte_buf_mask = 0;
+
+void print_buffer_status (void)
+{
+    int i;
+    unsigned char mask = 0x80;
+
+    printf ("      byte_buf_mask: ");
+    for (i = 0; i< NUM_BYTE_BUF; i++)
+    {
+        printf ("%d ", (byte_buf_mask & mask) >> (7-i));
+        mask = mask >> 1;
+    }
+
+    printf ("\n");
+}
+
+void our_malloc(int type, void **target, int *mem_location)
+{
+    int location;
+
+    if (byte_buf_mask == FULL){
+        return;
+    }
+    else if (type == TYPE_SMALL)
+    {
+        location = test_single_location(byte_buf_mask, NUM_BYTE_BUF);
+        set_single_bit(&byte_buf_mask, location);
+        *target = &buffer[location*ELEMENT_SIZE];
+        *mem_location = location;
+    }
+    else
+    {
+        location = test_dual_location(byte_buf_mask, NUM_BYTE_BUF);
+        if (location >= 0)
+        {
+            set_dual_bit(&byte_buf_mask, location);
+            *target = &buffer[location*ELEMENT_SIZE];
+            *mem_location = location;
+        }
+        else
+        {
+            return;
+        }
+    }    
+}
+
+int test_single_location(unsigned char mask, int mask_length)
+{
+    int loc; 
+    unsigned char test = 0x01;
+    for(loc=0;loc<mask_length;loc++)
+    {
+        if((test & mask) == 0)
+        {
+            return loc;  //0~mask_length-1
+        }
+        test = test << 1;
+    }
+}
+
+void set_single_bit(unsigned char *mask, int location)
+{
+    unsigned char set = 0x01;
+    int i; //loop control
+    for(i=0;i<location;i++)  //go to target bit
+    {
+        set = set << 1; 
+    }
+    *mask = *mask | set;
+}
+
+int test_dual_location(unsigned char mask, int mask_length)
+{
+    int loc; 
+    unsigned char test = 0x03;
+    for(loc=0;loc<mask_length-1;loc++)
+    {
+        if((test & mask) == 0)
+        {
+            return loc;  //loc & loc+1
+        }
+        test = test << 1;
+    }
+}
+
+void set_dual_bit(unsigned char *mask, int location)
+{
+    unsigned char set = 0x03;
+    int i; //loop control
+    for(i=0;i<location;i++)  //go to target bit
+    {
+        set = set << 1; 
+    }
+    *mask = *mask | set;
+}
+
+void clear_single_bit(unsigned char *mask, int location)
+{
+    unsigned char clear = 0x01;
+    int i; //loop control
+    for(i=0;i<location;i++)  //go to target bit
+    {
+        clear = clear << 1; 
+    }
+    *mask = *mask & (~clear);
+}
+
+void clear_dual_bit(unsigned char *mask, int location)
+{
+    unsigned char clear = 0x03;
+    int i; //loop control
+    for(i=0;i<location;i++)  //go to target bit
+    {
+        clear = clear << 1; 
+    }
+    *mask = *mask & (~clear);
+}
+
+void our_free(int type, int mem_location)
+{
+    if (type == TYPE_SMALL)
+    {
+        unsigned char clear = 0x01;
+        for(int i=0;i<mem_location;i++)  //go to target bit
+        {
+            clear = clear << 1; 
+        }
+        byte_buf_mask = byte_buf_mask & (~clear);
+    }
+    else if (type == TYPE_LARGE)
+    {
+        unsigned char clear = 0x03;
+        for(int i=0;i<mem_location;i++)  //go to target bit
+        {
+            clear = clear << 1; 
+        }
+        byte_buf_mask = byte_buf_mask & (~clear);
+    }
+}
